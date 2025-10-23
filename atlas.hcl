7fd5f9dd19a551769d09507570e4c740c9e7707b
Smith Shelke <smith.shelke@grofers.com>
2025-10-23 12:42:05 +0530

    Use production postgres

--- atlas.hcl
+++ atlas.hcl
@@ -1,4 +1,13 @@
 env "dev" {
   url = "postgres://user:password@db:5432/shelke_dev_api?sslmode=disable"
-  dev = "postgres://user:password@db:5432/shelke_dev_api?sslmode=disable"
+  migration {
+    dir = "file://db/migrations"
+  }
+}
+
+env "prod" {
+  url = env("DB_URL")
+  migration {
+    dir = "file://db/migrations"
+  }
 }
--- internal/adapters/db/db.go
+++ internal/adapters/db/db.go
@@ -3,13 +3,19 @@ package db
 import (
 	"context"
 	"fmt"
+	"os" // Import the os package
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	db "shelke.dev/api/db/sqlc"
 )
 
 func NewDB() (*db.Queries, *pgxpool.Pool, error) {
-	connStr := "postgres://user:password@localhost:5432/shelke_dev_api?sslmode=disable"
+	connStr := os.Getenv("DB_URL") // Read from environment variable
+	if connStr == "" {
+		// Use default if DB_URL is not set
+		connStr = "postgres://user:password@localhost:5432/shelke_dev_api?sslmode=disable"
+	}
+
 	pool, err := pgxpool.New(context.Background(), connStr)
 	if err != nil {
 		return nil, nil, fmt.Errorf("Unable to connect to database: %v", err)
